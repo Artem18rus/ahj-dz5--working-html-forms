29350b1b291e858f8c9cc75f0a6888d17b6badf1
AppVeyor <urart88@mail.ru>
2022-11-18 20:09:16 +0400

    5M

--- src/js/__test__/widget.test.js
+++ src/js/__test__/widget.test.js
@@ -5,7 +5,7 @@ import ClassClick from '../app';
 test('widget', () => {
   document.body.innerHTML = '<div class="container"></div>';
 
-  const container = document.querySelector('.container');
+  //const container = document.querySelector('.container');
   const widget = ClassClick.displayOnClickMethod();
 
   widget.bindToDOM();
